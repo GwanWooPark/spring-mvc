dbff6a601250ae21877d62c21acdbff203353d9d
GwanWooPark <pgw4712@icloud.com>
2022-02-17 01:24:38 +0900

    FrontController V1

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -16,5 +16,6 @@ public class MemberFormControllerV1 implements ControllerV1 {
         String viewPath = "/WEB-INF/views/new-form.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+
     }
 }
